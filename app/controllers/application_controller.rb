6a732defee3ea1e690a0aa76cf165a20ea990564
John Werenski <jtwerenski@gmail.com>
2018-12-18 22:10:22 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,7 +30,7 @@ class ApplicationController < Sinatra::Base
 
   post '/sessions' do
     @user = User.find_by(email: params[:email], password: params[:password])
-    if @user.id == nil 
+    if @user == nil 
       redirect '/sessions/login'
     end 
     session[:id] = @user.id 
