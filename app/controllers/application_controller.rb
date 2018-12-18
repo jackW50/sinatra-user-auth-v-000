340688c0a7b4a54e37a6403fcc75f26fe7ae0ff9
John Werenski <jtwerenski@gmail.com>
2018-12-18 21:35:00 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,7 +29,7 @@ class ApplicationController < Sinatra::Base
 
   post '/sessions' do
     @user = User.find_by(password: params[:password])
-  
+    @user.id = session[:id]
     redirect '/users/home'
   end
 
